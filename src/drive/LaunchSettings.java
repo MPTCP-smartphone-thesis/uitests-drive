9cec1ec3e60a04543a2350e2a30f5f1112da2417
Quentin De Coninck <quentin.deconinck@student.uclouvain.be>
2014-09-23 17:31:27 +0200

    [ADD] Beginning of Drive UI test, must be continued

---
+++ src/drive/LaunchSettings.java
@@ -0,0 +1,44 @@
+package drive;
+import java.util.ArrayList;
+import java.util.List;
+
+import shazam.Utils;
+import login.LoginClass;
+
+import com.android.uiautomator.testrunner.UiAutomatorTestCase;
+import com.android.uiautomator.core.UiObject;
+import com.android.uiautomator.core.UiObjectNotFoundException;
+import com.android.uiautomator.core.UiSelector;
+
+
+public class LaunchSettings extends UiAutomatorTestCase {
+	
+	private static String ID_DRIV_UPLOAD = "com.google.android.apps.docs:id/create_bar_upload";
+	
+	private void createFile() {
+		String[] commands = {"cd /storage/sdcard0", "dd if=/dev/urandom of=/storage/sdcard0/random_seed bs=1 count=100000", "cat /storage/sdcard0/random_seed /storage/sdcard0/random_seed_orig /storage/sdcard0/random_seed > /storage/sdcard0/random_seed_concat"};
+		Utils.runAsRoot(commands);
+	}
+	
+	private void updateFile() {
+		Utils.clickAndWaitForNewWindow(ID_DRIV_UPLOAD);
+		
+		sleep(1000);
+		
+		UiObject internal_storage = new UiObject(new UiSelector()
+			.className("android.widget.LinearLayout").instance(5));
+		Utils.click(internal_storage);
+	}
+	
+	
+	public void testDemo() throws UiObjectNotFoundException{
+		assertTrue("OOOOOpps",Utils.openApp(this, "Drive", "com.google.android.apps.docs"));
+		
+		//while(true) {
+			createFile();
+			updateFile();
+		//}
+		
+	}
+
+}
