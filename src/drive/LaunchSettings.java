d16b730a47c825a967d863395f5d0df853401f93
Matthieu Baerts <matttbe@gmail.com>
2014-09-23 23:17:11 +0200

    LaunchSettings: Used formatter option

--- src/drive/LaunchSettings.java
+++ src/drive/LaunchSettings.java
@@ -1,39 +1,43 @@
 package drive;
 
+import utils.Utils;
+
 import com.android.uiautomator.core.UiObject;
 import com.android.uiautomator.core.UiObjectNotFoundException;
 import com.android.uiautomator.core.UiSelector;
 import com.android.uiautomator.testrunner.UiAutomatorTestCase;
 
-
 public class LaunchSettings extends UiAutomatorTestCase {
-	
+
 	private static String ID_DRIV_UPLOAD = "com.google.android.apps.docs:id/create_bar_upload";
-	
+
 	private void createFile() {
-		String[] commands = {"cd /storage/sdcard0", "dd if=/dev/urandom of=/storage/sdcard0/random_seed bs=1 count=100000", "cat /storage/sdcard0/random_seed /storage/sdcard0/random_seed_orig /storage/sdcard0/random_seed > /storage/sdcard0/random_seed_concat"};
+		String[] commands = {
+				"cd /storage/sdcard0",
+				"dd if=/dev/urandom of=/storage/sdcard0/random_seed bs=1 count=100000",
+				"cat /storage/sdcard0/random_seed /storage/sdcard0/random_seed_orig /storage/sdcard0/random_seed > /storage/sdcard0/random_seed_concat" };
 		Utils.runAsRoot(commands);
 	}
-	
+
 	private void updateFile() {
 		Utils.clickAndWaitForNewWindow(ID_DRIV_UPLOAD);
-		
+
 		sleep(1000);
-		
-		UiObject internal_storage = new UiObject(new UiSelector()
-			.className("android.widget.LinearLayout").instance(5));
+
+		UiObject internal_storage = new UiObject(new UiSelector().className(
+				"android.widget.LinearLayout").instance(5));
 		Utils.click(internal_storage);
 	}
-	
-	
-	public void testDemo() throws UiObjectNotFoundException{
-		assertTrue("OOOOOpps",Utils.openApp(this, "Drive", "com.google.android.apps.docs"));
-		
-		//while(true) {
-			createFile();
-			updateFile();
-		//}
-		
+
+	public void testDemo() throws UiObjectNotFoundException {
+		assertTrue("OOOOOpps",
+				Utils.openApp(this, "Drive", "com.google.android.apps.docs"));
+
+		// while(true) {
+		createFile();
+		updateFile();
+		// }
+
 	}
 
 }
