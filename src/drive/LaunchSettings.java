505ed3b824c25c2edc6083b8c52af8df142fc9d7
Quentin De Coninck <quentin.deconinck@student.uclouvain.be>
2014-09-24 11:26:14 +0200

    [IMP] Drive UI Test is now functional

--- src/drive/LaunchSettings.java
+++ src/drive/LaunchSettings.java
@@ -1,15 +1,23 @@
 package drive;
 
+import java.util.List;
+
 import utils.Utils;
 
 import com.android.uiautomator.core.UiObject;
 import com.android.uiautomator.core.UiObjectNotFoundException;
+import com.android.uiautomator.core.UiScrollable;
 import com.android.uiautomator.core.UiSelector;
 import com.android.uiautomator.testrunner.UiAutomatorTestCase;
 
 public class LaunchSettings extends UiAutomatorTestCase {
 
 	private static String ID_DRIV_UPLOAD = "com.google.android.apps.docs:id/create_bar_upload";
+	private static String ID_DRIV_LIST = "com.android.documentsui:id/list";
+	private static String ID_DRIV_NAME = "android:id/title";
+	private static String ID_DRIV_TITLE = "android:id/action_bar_title";
+	private static String LABEL_OPEN_FROM = "Open from";
+	private static String LABEL_MY_DRIVE = "My Drive";
 
 	private void createFile() {
 		String[] commands = {
@@ -20,23 +28,74 @@ public class LaunchSettings extends UiAutomatorTestCase {
 	}
 
 	private void updateFile() {
+		// Be more adaptative...
+		if (!Utils.hasText(ID_DRIV_TITLE, LABEL_MY_DRIVE)) {
+			getUiDevice().pressBack();
+		}
+
 		Utils.clickAndWaitForNewWindow(ID_DRIV_UPLOAD);
 
 		sleep(1000);
 
-		UiObject internal_storage = new UiObject(new UiSelector().className(
-				"android.widget.LinearLayout").instance(5));
-		Utils.click(internal_storage);
+		// A little tricky to select the button...
+		// Not always to have that, verify if needed first
+		UiObject action_title = new UiObject(
+				new UiSelector().resourceId(ID_DRIV_TITLE));
+		
+		if (Utils.hasText(action_title, LABEL_OPEN_FROM)) {
+
+			UiObject internal_storage = new UiObject(
+					new UiSelector()
+							.className("android.widget.ListView")
+							.instance(0)
+							.childSelector(
+									new UiSelector().className(
+											"android.widget.LinearLayout")
+											.instance(10)));
+			Utils.click(internal_storage);
+
+		}
+
+		sleep(1000);
+
+		// Scroll the view until finding our file
+		boolean found = false;
+		UiScrollable list = new UiScrollable(
+				new UiSelector().resourceId(ID_DRIV_LIST));
+		
+		String[] toFind = { "random_seed_concat" };
+
+		while (!found) {
+			List<UiObject> available = Utils.getElems(ID_DRIV_LIST,
+					ID_DRIV_NAME);
+			assertTrue("Unable to retrieve the list", !available.isEmpty());
+
+			for (UiObject dest : available) {
+				for (String tf : toFind) {
+					if (Utils.hasText(dest, tf)) {
+						assertTrue("Unable to select element",
+								Utils.hasTextAndClick(dest, tf));
+						found = true;
+					}
+				}
+			}
+
+			if (!found) {
+				Utils.scrollForward(list);
+			}
+		}
+
 	}
 
 	public void testDemo() throws UiObjectNotFoundException {
 		assertTrue("OOOOOpps",
 				Utils.openApp(this, "Drive", "com.google.android.apps.docs"));
 
-		// while(true) {
-		createFile();
-		updateFile();
-		// }
+		while (true) {
+			createFile();
+			updateFile();
+			sleep(60000);
+		}
 
 	}
 
